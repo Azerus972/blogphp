b0ae2ec03a9c9b0e93e9343f55f52a12e897f8fc
Azerus <kevin.marous@gmail.com>
2017-05-31 12:33:33 +0200

    done

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<?php
+
+
+$log_directory = 'posts';
+
+$results_array = array();
+
+if (is_dir($log_directory))
+{
+        if ($handle = opendir($log_directory))
+        {
+                
+                while(($file = readdir($handle)) !== FALSE)
+                {
+                        $results_array[] = $file;
+                }
+                closedir($handle);
+        }
+}
+
+
+foreach($results_array as $value)
+{
+    echo $value . '<br />';
+}
+
+
+
+
+?>
+
+
+
+</body>
+</html>
