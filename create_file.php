f590f8190929728e1d85903767c0fb710bc6e9d7
Azerus <kevin.marous@gmail.com>
2017-05-31 10:42:30 +0200

    test

---
+++ create_file.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>create</title>
+</head>
+<body>
+    
+<?php
+
+
+$textzone= $GET_['textzone'];
+$file = fopen("textzone.txt", "w");
+fwrite($file, $textzone);
+fclose($file);
+
+
+
+
+?>
+
+
+</body>
+</html>
