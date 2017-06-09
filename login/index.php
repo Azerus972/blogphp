4c965cb4a7b1232a85008c7f081b64142c6626d4
Azerus <kevin.marous@gmail.com>
2017-06-09 12:19:28 +0200

    style + bug fixed * still working on it....

--- login/index.php
+++ login/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <link rel="stylesheet" type="text/css" href="index.css">
     <title>Document</title>
 </head>
 
@@ -12,7 +13,7 @@
 
 <?php
 $msg ="Votre espace personnel est en cours de chargement la page va s'acutaliser automatiquement...";
-header('Refresh: 2; URL=../create.html');
+header('Refresh: 4; URL=../create.html');
 ?>
 
 
@@ -20,6 +21,11 @@ header('Refresh: 2; URL=../create.html');
 </header>
 
 <body>
+
+
+
+
+
     <h1><strong> Félicitation Vous êtes connecté!</strong></h1>
 
 
--- login/login.php
+++ login/login.php
@@ -5,11 +5,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+     <link rel="stylesheet" type="text/css" href="login.css">
 
     <title>Login My Blog</title>
+
 </head>
 
+<header>
+
+
+<nav>
+            <ul>
+                <li><a href="login.php">Connexion</a></li>
+                <li><a href="mailto:kevin.marous@gmail.com">CONTACT</a></li>
+            </ul>
+</nav>
+
+
+</header>
+
 <body>
+
     <style>
         body {
             background-color: grey;
@@ -17,6 +33,7 @@
             flex-direction: column;
         }
     </style>
+
 
     <form action="auth.php" method="POST">
 
